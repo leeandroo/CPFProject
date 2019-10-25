487645fd2c2482c843bc90db7778a8f19fcc08d7
Leandro Cisterna <leandrocisterna7@gmail.com>
2019-10-25 11:51:40 -0300

    correción código para calendario

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -164,34 +164,11 @@
     <script type="text/javascript" src="{{asset('js/mdb.js')}}"></script>
     <script type="text/javascript" src="{{asset('js/main.js')}}"></script>
 
-	
+	<script type="text/javascript" src="{{asset('js/scripts2.js')}}"></script>
 	<script language="javascript"  type="text/javascript" src="{{asset('js/dropdown.js')}}" ></script>
-	
-
     <script>
       // SideNav Initialization
       $(".button-collapse").sideNav();	  
-    </script>
-    <script>
-	  	$('#message').modal('show')
-	  
-      	$("#datepicker").datepicker({
-			changeyear: true,
-			closeText: 'Cerrar',
-			prevText: '< Ant',
-			nextText: 'Sig >',
-			currentText: 'Hoy',
-			monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-			monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
-			dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-			dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
-			dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
-			weekHeader: 'Sm',
-			dateFormat: 'dd/mm/yy',
-			firstDay: 1,
-				maxDate: 0
-		});
-	
     </script>
 </body>
 </html>
